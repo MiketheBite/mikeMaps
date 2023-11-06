d7b2f9557b12a6953c119ed4a5fcf1f30a3474e7
MiketheBite <migueljcpinto@mail.com>
2023-11-06 23:32:09 +0100

    emojis

--- src/components/City/City.jsx
+++ src/components/City/City.jsx
@@ -35,7 +35,7 @@ function City() {
       <div className={styles.row}>
         <h6>City name</h6>
         <h3>
-          <span>{emoji}</span> {cityName}
+          <span>{emoji ? flagemojiToPNG(emoji) : ""}</span> {cityName}
         </h3>
       </div>
 
--- src/components/Country/CountryItem.jsx
+++ src/components/Country/CountryItem.jsx
@@ -1,9 +1,12 @@
+import { useCities } from "../../contexts/CitiesContext";
 import styles from "./CountryItem.module.css";
 
 function CountryItem({ country }) {
+  const { flagemojiToPNG } = useCities();
+
   return (
     <li className={styles.countryItem}>
-      <span>{country.emoji}</span>
+      <span>{country.emoji ? flagemojiToPNG(country.emoji) : ""}</span>
       <span>{country.country}</span>
     </li>
   );
