2d1c249cbcf1cd532a776403727715f14f9c6d0d
MiketheBite <migueljcpinto@mail.com>
2023-11-16 10:25:56 +0100

    adjust

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,6 @@ export default function App() {
               <Routes>
                 <Route index element={<Homepage />} />
                 <Route path="login" element={<Login />} />
-
                 <Route
                   path="app"
                   element={
@@ -38,13 +37,11 @@ export default function App() {
                   }
                 >
                   <Route index element={<Navigate replace to="cities" />} />
-
                   <Route path="cities" element={<CityList />} />
                   <Route path="cities/:id" element={<City />} />
                   <Route path="countries" element={<CountryList />} />
                   <Route path="form" element={<Form />} />
                 </Route>
-
                 <Route path="*" element={<PageNotFound />} />
               </Routes>
             </Suspense>
